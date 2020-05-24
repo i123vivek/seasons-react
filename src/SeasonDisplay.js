3c2dd188da00f4254f2961c9287d8acb083fc4c0
vivek <i123vivek@gmail.com>
2020-05-25 02:14:07 +0530

    displaying season based on months and latitude@react

---
+++ src/SeasonDisplay.js
@@ -0,0 +1,47 @@
+import './SeasonDisplay.css'
+import React from 'react'
+
+const seasonConfig = {
+    Summer: {
+        text : "Let's hit the beach",
+        iconName : 'sun'
+    },
+    Winter: {
+        text : 'Burr, it is chilly',
+        iconName : 'snowflake'
+    }
+}
+
+const getSeason = (lat, month) =>{
+
+    if(month > 2 && month < 9){
+        return lat > 0 ? 'Summer' : 'Winter';  
+    } else {
+        return lat > 0 ? 'Winter' : 'Summer';
+    }
+
+}
+
+const SeasonDisplay = (props) =>{
+
+    // console.log("latitude value is: ", props.lat);
+
+    const season = getSeason(props.lat, new Date().getMonth());
+
+    const {text, iconName} = seasonConfig[season];
+
+    //console.log("Season is :",season);
+
+    // const text = season === 'Winter' ? 'Burr, it is chilly' : "Let's hit the beach";
+
+    // const icon = season === 'Winter' ? 'snowflake' : 'sun';
+    return (
+        <div className = {`season-display ${season}`}>
+            <i className = {`icon-left massive ${iconName} icon`}/>
+            <h1>{text}</h1>
+            <i className = {`icon-right massive ${iconName} icon`}/>
+        </div>
+    );
+}
+
+export default SeasonDisplay;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,61 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import SeasonDisplay from './SeasonDisplay';
+import Spinner from './Spinner';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// const App = () =>{
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+//     window.navigator.geolocation.getCurrentPosition(
+//         position => console.log(position),
+//         err => console.log(err)
+//     );
+
+//     return (
+//         <div className="text">
+//             Hi React!
+//         </div>
+//     );
+// }
+
+
+class App extends React.Component {
+
+    // constructor(props) {
+    //     super(props);
+
+    //     this.state = { lat: null, errorMessage: '' };        
+    // }
+
+    // or
+
+    state = { lat: null, errorMessage : ''};
+
+
+    componentDidMount() {
+        window.navigator.geolocation.getCurrentPosition(
+            position => {
+                this.setState({ lat: position.coords.latitude })
+            },
+            err => {
+                this.setState({ errorMessage: err.message })
+            }
+        );
+    }
+
+    renderContent(){
+        if (this.state.errorMessage && !this.state.lat) {
+            return <div> Error: {this.state.errorMessage}</div>;
+        } else if (!this.state.errorMessage && this.state.lat) {
+            return <SeasonDisplay lat={this.state.lat}/>
+        } else {
+            return <Spinner message = "Please accept location request" />
+        }
+    }
+
+    // react says we have to define  render method . If we don't do so react is going to throw an error.
+    render() {
+        return <div className = "border red">{this.renderContent()}</div>
+    }
+}
+
+ReactDOM.render(<App />, document.querySelector('#root'));
